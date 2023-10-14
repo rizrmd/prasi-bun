5f7a7ed419698367fca0a2be5b48ccf365ddb5c4
Rizky <rizky05@gmail.com>
2023-10-14 15:17:49 +0700

    fix bun

--- app/srv/api/session.ts
+++ app/srv/api/session.ts
@@ -1,9 +1,37 @@
 import { apiContext } from "service-srv";
+import { session } from "utils/session";
+import { user } from "dbgen";
 
 export const _ = {
-  url: "/session)}",
+  url: "/session",
   async api() {
     const { req, res } = apiContext(this);
-    return "This is session.ts";
-  }
-}
+    const sdata = session.get<{
+      user: user & {
+        org: {
+          id: string;
+          name: string;
+        }[];
+      };
+    }>(req);
+    if (sdata) {
+      let setDefaultCookie = true;
+      const origin = req.headers.get("origin");
+      if (origin) {
+        const url = new URL(origin);
+        if (url.hostname === "localhost") {
+          setDefaultCookie = false;
+          res.setHeader("set-cookie", `${session.cookieKey}=${sdata.id};`);
+        }
+      }
+
+      if (setDefaultCookie) {
+        res.setHeader(
+          "set-cookie",
+          `${session.cookieKey}=${sdata.id}; SameSite=None; Secure; HttpOnly`
+        );
+      }
+    }
+    return sdata;
+  },
+};
