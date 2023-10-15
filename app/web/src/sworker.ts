fee3077e4dc764920bd1dec624996d228bf7c45f
Rizky <rizky05@gmail.com>
2023-10-15 13:55:40 +0700

    fix

--- app/web/src/sworker.ts
+++ app/web/src/sworker.ts
@@ -19,41 +19,41 @@ addEventListener("activate", (e) =>
   (e as ExtendableEvent).waitUntil(activate())
 );
 
-// addEventListener("fetch", async (evt) => {
-//   const e = evt as FetchEvent;
+addEventListener("fetch", async (evt) => {
+  const e = evt as FetchEvent;
 
-//   e.respondWith(
-//     (async () => {
-//       if (!g.cache) {
-//         g.cache = await caches.open(version);
-//       }
-//       const cache = g.cache;
-//       const r = await cache.match(e.request);
-//       if (r) {
-//         return r;
-//       }
-//       const url = e.request.url;
-//       if (url.includes("_api_frm")) {
-//         cache.add(e.request);
-//       }
+  e.respondWith(
+    (async () => {
+      if (!g.cache) {
+        g.cache = await caches.open(version);
+      }
+      const cache = g.cache;
+      const r = await cache.match(e.request);
+      if (r) {
+        return r;
+      }
+      const url = e.request.url;
+      if (url.includes("_api_frm")) {
+        cache.add(e.request);
+      }
 
-//       return await fetch(e.request);
-//     })()
-//   );
-// });
-// addEventListener("message", async (e) => {
-//   const type = e.data.type;
+      return await fetch(e.request);
+    })()
+  );
+});
+addEventListener("message", async (e) => {
+  const type = e.data.type;
 
-//   if (!g.cache) {
-//     g.cache = await caches.open(version);
-//   }
-//   const cache = g.cache;
+  if (!g.cache) {
+    g.cache = await caches.open(version);
+  }
+  const cache = g.cache;
 
-//   switch (type) {
-//     case "add-cache":
-//       if (!(await cache.match(e.data.url))) {
-//         await cache.add(e.data.url);
-//       }
-//       break;
-//   }
-// });
+  switch (type) {
+    case "add-cache":
+      if (!(await cache.match(e.data.url))) {
+        await cache.add(e.data.url);
+      }
+      break;
+  }
+});
