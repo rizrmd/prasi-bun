a409814e6e958f9ba6e76bdc4340cc8f483c8743
Rizky <rizky05@gmail.com>
2023-10-16 11:52:34 +0700

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 bun run build
-ssh plansys@web.andromedia.co.id "cd /home/plansys/prasi/app && git pull && /home/plansys/.bun/bin/bun run pull"
+ssh plansys@web.andromedia.co.id "cd /home/plansys/prasi/app && bun run deploy"
