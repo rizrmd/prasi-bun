674a39fdca3e33880e88444518386f03688ed34c
Rizky <rizky05@gmail.com>
2023-10-16 20:52:51 +0700

    kebalik

--- app/srv/exports.ts
+++ app/srv/exports.ts
@@ -5,26 +5,19 @@ export const npm_size = {
   args: ["mode","id"],
   handler: import("./api/npm-size")
 }
+export const login = {
+  name: "login",
+  url: "/_login",
+  path: "app/srv/api/auth/login.ts",
+  args: ["username","password"],
+  handler: import("./api/auth/login")
+}
 export const session = {
   name: "session",
-  url: "/_session",
-  path: "app/srv/api/auth/session.ts",
+  url: "/session",
+  path: "app/srv/api/session.ts",
   args: [],
-  handler: import("./api/auth/session")
-}
-export const site_dts = {
-  name: "site_dts",
-  url: "/site-dts/:site_id",
-  path: "app/srv/api/site-dts.ts",
-  args: ["site_id"],
-  handler: import("./api/site-dts")
-}
-export const npm_bundle = {
-  name: "npm_bundle",
-  url: "/npm-bundle/:mode/:id",
-  path: "app/srv/api/npm-bundle.ts",
-  args: ["mode","id"],
-  handler: import("./api/npm-bundle")
+  handler: import("./api/session")
 }
 export const npm = {
   name: "npm",
@@ -33,19 +26,26 @@ export const npm = {
   args: ["mode","id"],
   handler: import("./api/npm")
 }
-export const login = {
-  name: "login",
-  url: "/_login",
-  path: "app/srv/api/auth/login.ts",
-  args: ["username","password"],
-  handler: import("./api/auth/login")
-}
 export const local_ip = {
   name: "local_ip",
   url: "/local-ip",
   path: "app/srv/api/local-ip.ts",
   args: [],
   handler: import("./api/local-ip")
+}
+export const npm_bundle = {
+  name: "npm_bundle",
+  url: "/npm-bundle/:mode/:id",
+  path: "app/srv/api/npm-bundle.ts",
+  args: ["mode","id"],
+  handler: import("./api/npm-bundle")
+}
+export const site_dts = {
+  name: "site_dts",
+  url: "/site-dts/:site_id",
+  path: "app/srv/api/site-dts.ts",
+  args: ["site_id"],
+  handler: import("./api/site-dts")
 }
 export const _upload = {
   name: "_upload",
@@ -61,6 +61,13 @@ export const _prasi = {
   args: [],
   handler: import("./api/_prasi")
 }
+export const _file = {
+  name: "_file",
+  url: "/_file/*",
+  path: "app/srv/api/_file.ts",
+  args: [],
+  handler: import("./api/_file")
+}
 export const _api_frm = {
   name: "_api_frm",
   url: "/_api_frm",
@@ -74,11 +81,4 @@ export const _dbs = {
   path: "app/srv/api/_dbs.ts",
   args: ["dbName","action"],
   handler: import("./api/_dbs")
-}
-export const _file = {
-  name: "_file",
-  url: "/_file/*",
-  path: "app/srv/api/_file.ts",
-  args: [],
-  handler: import("./api/_file")
 }
