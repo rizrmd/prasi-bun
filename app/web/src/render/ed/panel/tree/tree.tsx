359aad783c652bc82d1d938c9fb2517d1fb7d927
Rizky <rizky05@gmail.com>
2023-10-24 21:16:35 +0700

    fix ui

--- app/web/src/render/ed/panel/tree/tree.tsx
+++ app/web/src/render/ed/panel/tree/tree.tsx
@@ -10,7 +10,7 @@ export const EdTree = () => {
     <div className="flex flex-col min-w-[300px] relative border-r">
       <EdTreeSearch />
       <div className="tree-body flex relative flex-1 overflow-y-auto overflow-x-hidden">
-        <div className="absolute inset-0">
+        <div className="absolute inset-0 flex">
           <DndProvider backend={MultiBackend} options={getBackendOptions()}>
             <EdTreeBody />
           </DndProvider>
