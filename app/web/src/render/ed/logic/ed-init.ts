62df8a3faafaeb9a7429247891b26f9640d14f2b
Rizky <rizky05@gmail.com>
2023-10-25 20:13:25 +0700

    fix

--- app/web/src/render/ed/logic/ed-init.ts
+++ app/web/src/render/ed/logic/ed-init.ts
@@ -1,8 +1,10 @@
+import init from "wasm-gzip";
 import { PG } from "./ed-global";
-import init, { decompress } from "wasm-gzip";
+import { jscript } from "../../editor/panel/script/script-element";
 
 export const edInit = async (p: PG) => {
   await init();
+  jscript.init();
   p.status = "ready";
   p.render();
 };
