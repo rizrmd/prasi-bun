8b7d16ed7d19b3f4a53ed553375844747c0f07f5
Rizky <rizky05@gmail.com>
2023-11-02 12:39:55 +0700

    fix

--- app/srv/ws/sync/type.ts
+++ app/srv/ws/sync/type.ts
@@ -1,23 +1,7 @@
 import { ServerWebSocket } from "bun";
-import { UserConf } from "./entity/user";
 import { WSData } from "../../../../pkgs/core/server/create";
 import { user } from "../../../db/db";
-
-export enum Activity {
-  Open,
-  Null,
-}
-
-export type PAGE_ID = string;
-export type COMP_ID = string;
-export type ITEM_ID = string;
-export type CLIENT_ID = string;
-
-export type ActivityKind = "root" | "js" | "css" | "html" | "text";
-export type ActivityList = Record<
-  ITEM_ID,
-  Partial<Record<ActivityKind, Record<CLIENT_ID, Activity>>>
->;
+import { UserConf } from "./entity/user";
 
 export enum SyncType {
   ClientID,
