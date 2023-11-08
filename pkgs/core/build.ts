fc3d9725358b29bb2380074a101ef6e3f18e3bc0
Rizky <me@rizrmd.com>
2023-11-08 22:12:36 +0000

    fix build site

--- pkgs/core/build.ts
+++ pkgs/core/build.ts
@@ -1,17 +1,14 @@
 import brotliPromise from "brotli-wasm";
 import { spawn } from "bun";
 import { dir } from "dir";
-import { Plugin, context } from "esbuild";
-import { $ } from "execa";
 import { fdir } from "fdir";
 import { statSync } from "fs";
 import {
+  copyAsync,
+  existsAsync,
   listAsync,
   removeAsync,
   writeAsync,
-  inspectTree,
-  existsAsync,
-  copyAsync,
 } from "fs-jetpack";
 const brotli = await brotliPromise;
 
@@ -101,54 +98,4 @@ if (files) {
   }
 }
 
-const buildSite = async () => {
-  await removeAsync(dir.path("app/srv/site"));
-  const onEndPlugin: Plugin = {
-    name: "on-end",
-    setup(build) {
-      build.onEnd(async (result) => {
-        console.log("Compressing deploy");
-        await removeAsync(dir.path("app/srv/site.zip"));
-        await $({ cwd: dir.path("app/srv") })`zip -r site.zip site`;
-        process.exit(0);
-      });
-    },
-  };
-
-  console.log("Building deploy");
-  const ctx = await context({
-    bundle: true,
-    absWorkingDir: dir.path(""),
-    entryPoints: [dir.path("app/web/src/render/site/site.tsx")],
-    outdir: dir.path("app/srv/site"),
-    splitting: true,
-    format: "esm",
-    jsx: "transform",
-    minify: true,
-    sourcemap: true,
-    logLevel: "error",
-    plugins: [onEndPlugin],
-    define: {
-      "process.env.NODE_ENV": `"production"`,
-    },
-  });
-  await ctx.rebuild();
-  await writeAsync(
-    dir.path("app/srv/site/index.html"),
-    `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title></title>
-  <link rel="stylesheet" href="https://prasi.app/index.css">
-</head>
-<body class="flex-col flex-1 w-full min-h-screen flex opacity-0">
-  <div id="root"></div>
-  <script src="/site.js" type="module"></script>
-</body>
-</html>`
-  );
-};
-await buildSite();
+await import("./build-site");
