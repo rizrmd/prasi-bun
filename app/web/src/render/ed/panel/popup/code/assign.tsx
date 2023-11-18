c19505692a451b0f3b920886323cd778e9957be2
Rizky <rizky05@gmail.com>
2023-11-18 15:48:00 +0700

    fix

--- app/web/src/render/ed/panel/popup/code/assign.tsx
+++ app/web/src/render/ed/panel/popup/code/assign.tsx
@@ -3,11 +3,29 @@ import { useGlobal, useLocal } from "web-utils";
 import { EDGlobal } from "../../../logic/ed-global";
 import { Popover } from "../../../../../utils/ui/popover";
 import { fuzzy } from "../../../../../utils/ui/fuzzy";
+import { iconTrash } from "./icons";
 
 const assign = {
   loading: true,
   list: [] as Awaited<ReturnType<typeof getAssign>>,
   render: () => {},
+};
+const select = {
+  id: true,
+  id_code: true,
+  id_component_group: true,
+  id_page: true,
+  page: {
+    select: {
+      name: true,
+      url: true,
+    },
+  },
+  component_group: {
+    select: {
+      name: true,
+    },
+  },
 };
 
 export const CodeAssign: FC<{ id_code: string; onClose: () => void }> = ({
@@ -41,6 +59,7 @@ const AssignList: FC<{ id_code: string; mode: "page" | "comp" }> = ({
       list: [] as { id: string; name: string; url?: string }[],
       popover: false,
       loading: true,
+      input: null as HTMLInputElement | null,
     },
     async () => {
       if (mode === "page") {
@@ -83,14 +102,29 @@ const AssignList: FC<{ id_code: string; mode: "page" | "comp" }> = ({
     }
   );
 
-  const filtered = fuzzy(
+  let filtered = fuzzy(
     local.list,
     mode === "page" ? { pk: "id", search: ["name", "url"] } : "name",
     local.search
   );
 
+  if (mode === "page") {
+    filtered = filtered.filter(
+      (e) => !assign.list.find((f) => f.id_page === e.id)
+    );
+  } else {
+    filtered = filtered.filter(
+      (e) => !assign.list.find((f) => f.id_component_group === e.id)
+    );
+  }
+
   return (
-    <div className="flex flex-col min-w-[300px] min-h-[100px] items-stretch">
+    <div
+      className={cx(
+        `flex flex-col min-h-[300px] items-stretch`,
+        mode === "page" ? "min-w-[600px]" : "min-w-[300px]"
+      )}
+    >
       <Popover
         open={local.popover}
         onOpenChange={(open) => {
@@ -128,6 +162,31 @@ const AssignList: FC<{ id_code: string; mode: "page" | "comp" }> = ({
 
                 return (
                   <div
+                    onClick={async () => {
+                      local.popover = false;
+                      local.render();
+
+                      let res = null as any;
+                      if (mode === "page") {
+                        res = await db.code_assign.create({
+                          data: {
+                            id_code: id_code,
+                            id_page: e.id,
+                          },
+                          select,
+                        });
+                      } else {
+                        res = await db.code_assign.create({
+                          data: {
+                            id_code: id_code,
+                            id_component_group: e.id,
+                          },
+                          select,
+                        });
+                      }
+                      assign.list.push(res);
+                      assign.render();
+                    }}
                     className={
                       "border-b p-1 flex flex-col items-stretch hover:bg-blue-100 cursor-pointer"
                     }
@@ -144,19 +203,66 @@ const AssignList: FC<{ id_code: string; mode: "page" | "comp" }> = ({
           <input
             type="text"
             value={local.search}
+            ref={(e) => {
+              if (e) {
+                local.input = e;
+              }
+            }}
             onChange={(e) => {
+              local.popover = true;
               local.search = e.currentTarget.value;
               local.render();
             }}
             spellCheck={false}
-            placeholder={`Search ${
+            placeholder={`+ Attach ${
               mode === "page" ? "Page" : "Component Group"
             }...`}
             className="flex flex-1 outline-none p-1 text-sm focus:border-blue-500 border border-transparent"
           />
         </div>
       </Popover>
-      <div className=""></div>
+      <div className="max-h-[500px] overflow-auto">
+        {assign.list.map((e) => {
+          if (
+            (mode === "page" && e.id_page) ||
+            (mode === "comp" && e.id_component_group)
+          ) {
+            return (
+              <div className="flex bg-white pl-2 border-b hover:bg-blue-50 items-stretch">
+                <div className="flex flex-1 py-1 pr-1">
+                  {(() => {
+                    if (mode === "page" && e.id_page) {
+                      return (
+                        <>
+                          <div>{e.page?.name}</div>
+                          <div className="flex-1 text-sm text-right text-slate-500">
+                            {e.page?.url}
+                          </div>
+                        </>
+                      );
+                    }
+                    if (mode === "comp" && e.id_component_group) {
+                      return <div>{e.component_group?.name}</div>;
+                    }
+                  })()}
+                </div>
+                <div
+                  className="border-l px-2 hover:bg-red-50 flex items-center justify-center hover:text-red-500 cursor-pointer"
+                  onClick={async () => {
+                    if (confirm("Are you sure ?")) {
+                      assign.list = assign.list.filter((f) => f.id !== e.id);
+                      assign.render();
+                      await db.code_assign.delete({ where: { id: e.id } });
+                    }
+                  }}
+                >
+                  <div dangerouslySetInnerHTML={{ __html: iconTrash }}></div>
+                </div>
+              </div>
+            );
+          }
+        })}
+      </div>
     </div>
   );
 };
@@ -165,6 +271,23 @@ const getAssign = async (id_code: string) => {
   return await db.code_assign.findMany({
     where: {
       id_code: { equals: id_code },
+    },
+    select: {
+      id: true,
+      id_code: true,
+      id_component_group: true,
+      id_page: true,
+      page: {
+        select: {
+          name: true,
+          url: true,
+        },
+      },
+      component_group: {
+        select: {
+          name: true,
+        },
+      },
     },
   });
 };
