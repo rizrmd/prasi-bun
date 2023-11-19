dd618e71a2de5f0bae0091565f555fd25bd4bf04
Rizky <rizky05@gmail.com>
2023-11-19 14:54:07 +0700

    fix

--- app/srv/api/nova-load.ts
+++ app/srv/api/nova-load.ts
@@ -4,7 +4,7 @@ import { g } from "utils/global";
 
 export const _ = {
   url: "/nova-load/:mode/:id/**",
-  async api(mode: "site" | "page" | "code", id: string) {
+  async api(mode: "site" | "code", id: string) {
     const { req, res } = apiContext(this);
 
     if (mode === "site") {
@@ -17,10 +17,6 @@ export const _ = {
           return new Response(file as any);
         }
       }
-    }
-    if (mode === "page") {
-      const code = await db.code_assign.findMany({ where: { id_page: id } });
-      return code.map((e) => e.id);
     } else if (mode === "code") {
       const file = Bun.file(
         dir.path(`${g.datadir}/site/build/${id}/${req.params["*"]}`)
