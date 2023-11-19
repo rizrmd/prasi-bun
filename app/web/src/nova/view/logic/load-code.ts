ea27006085608f0fb0f0411a96c2c23a3c88eea2
Rizky <rizky05@gmail.com>
2023-11-19 14:54:07 +0700

    fix

--- app/web/src/nova/view/logic/load-code.ts
+++ app/web/src/nova/view/logic/load-code.ts
@@ -1,6 +1,10 @@
 import { VG } from "./global";
 
 export const codeLoaded = new Set<string>();
+const codeMap = {
+  page: {} as Record<string, string[]>,
+  compGroup: {} as Record<string, string[]>,
+};
 export const vLoadCode = async (v: VG, forceLoad?: boolean) => {
   if (forceLoad) {
     codeLoaded.clear();
@@ -25,9 +29,57 @@ export const vLoadCode = async (v: VG, forceLoad?: boolean) => {
         resolve();
       }),
     ];
+    const code_ids: string[] = [];
+    const code = await db.code.findMany({
+      where: { id_site: site_id, name: { notIn: ["site", "SSR"] } },
+      select: { code_assign: true },
+    });
 
-    const code_ids = await api.nova_load("page", page_id);
-    for (const id of code_ids) {
+    codeMap.compGroup = {};
+    codeMap.page = {};
+    for (const c of code) {
+      c.code_assign.forEach((e) => {
+        if (e.id_page) {
+          if (!codeMap.page[e.id_page]) codeMap.page[e.id_page] = [];
+          codeMap.page[e.id_page].push(e.id_code);
+        }
+        if (e.id_component_group) {
+          if (!codeMap.compGroup[e.id_component_group])
+            codeMap.compGroup[e.id_component_group] = [];
+          codeMap.page[e.id_component_group].push(e.id_code);
+        }
+      });
+    }
+
+    if (codeMap.page[page_id]) {
+      for (const id of codeMap.page[page_id]) {
+        promises.push(
+          new Promise<void>(async (resolve) => {
+            if (!codeLoaded.has(id)) {
+              codeLoaded.add(id);
+              const module = await importCJS(`/nova-load/code/${id}/index.js`);
+              for (const [k, v] of Object.entries(module)) {
+                w[k] = v;
+              }
+            }
+            resolve();
+          })
+        );
+      }
+    }
+
+    await Promise.all(promises);
+
+    v.mode = "rebuild";
+    v.render();
+  }
+};
+
+const loadCGroupCode = async (cgroup_id: string) => {
+  if (codeMap.compGroup[cgroup_id]) {
+    const promises = [];
+    const w = window as any;
+    for (const id of codeMap.compGroup[cgroup_id]) {
       promises.push(
         new Promise<void>(async (resolve) => {
           if (!codeLoaded.has(id)) {
@@ -41,11 +93,7 @@ export const vLoadCode = async (v: VG, forceLoad?: boolean) => {
         })
       );
     }
-
     await Promise.all(promises);
-
-    v.mode = "rebuild";
-    v.render();
   }
 };
 
