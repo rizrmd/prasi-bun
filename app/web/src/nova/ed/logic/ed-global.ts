6c0f69a4a66dee8a34e25dad2afc4a0ed8c27de7
Rizky <rizky05@gmail.com>
2023-12-04 18:11:27 +0700

    wip draggable order

--- app/web/src/nova/ed/logic/ed-global.ts
+++ app/web/src/nova/ed/logic/ed-global.ts
@@ -195,7 +195,7 @@ export const EDGlobal = {
     group: {} as Record<string, Awaited<ReturnType<SAction["comp"]["group"]>>>,
   },
   ui: {
-    side: { prop: false },
+    side: { prop: true },
     layout: {
       left: parseInt(localStorage.getItem("prasi-layout-left") || "250"),
       right: parseInt(localStorage.getItem("prasi-layout-right") || "250"),
