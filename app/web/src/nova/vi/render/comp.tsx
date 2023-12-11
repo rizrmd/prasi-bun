4e2fa56cd99b068b7d4f2730a29e472508823af8
Rizky <rizky05@gmail.com>
2023-12-11 22:10:24 +0700

    wip fix script

---
+++ app/web/src/nova/vi/render/comp.tsx
@@ -0,0 +1,3 @@
+import { IMeta } from "../../ed/logic/ed-global";
+
+export const viEvalComp = (meta: IMeta) => {};
---
+++ app/web/src/nova/vi/render/script.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import { IMeta } from "../../ed/logic/ed-global";
+
+export const ViScript: FC<{ meta: IMeta }> = () => {
+  return <div></div>;
+};
