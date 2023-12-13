d2439ab042d6bb8df2cfacaf45d72301339dac7d
Rizky <rizky05@gmail.com>
2023-12-13 13:59:13 +0700

    fix

--- pkgs/core/server/api-ctx.ts
+++ pkgs/core/server/api-ctx.ts
@@ -41,9 +41,6 @@ const replacer = (key: string, value: string) => {
   if (typeof value === "bigint") {
     return `BigInt::${value}`;
   }
-  if (typeof value === "string" && value.startsWith("BigInt::")) {
-    return BigInt(value.substring(8));
-  }
   return value;
 };
 
