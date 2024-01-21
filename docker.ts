202f4f62ceb560afc9dae99c90624b48b8e9f27d
Rizky <rizky05@gmail.com>
2024-01-21 18:21:45 +0700

    fix

--- docker.ts
+++ docker.ts
@@ -1,12 +1,8 @@
-import { spawnSync } from "child_process";
-
 Bun.serve({
-  port: 3000,
+  port: 4550,
   fetch(request, server) {
     return new Response("Hello World\n\n" + process.cwd(), {
       status: 200,
     });
   },
 });
-
-spawnSync("bun run prod", { cwd: "/app/prasi/repo" });
