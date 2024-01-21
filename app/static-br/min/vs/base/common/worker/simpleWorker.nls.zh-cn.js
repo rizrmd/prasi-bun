0d05501039500e1decb85f5ff43eab72dc908a2b
Rizky <rizky05@gmail.com>
2024-01-21 12:59:19 +0700

    add static-br

---
+++ app/static-br/min/vs/base/common/worker/simpleWorker.nls.zh-cn.js
@@ -0,0 +1,5 @@
+ê ŒÃ8¼±rQ„"È€yõ•DÜ”Úªý¦Yc©ìÏ;nh³)Ð‘j]%ÁÀ9y­2Ãztãñ”Ár>èÁöAwA×Œä7A¦´á åçˆ\¤ý¤—MNê1Ô•—º>O}Ð7þé’†î8gCôI‚ÿ£øÿ7Å~ê'õ~Yÿß¶Tà«šfæ®-Y
+Öá³÷²	
+OÒÊõ¥f–µxùÞ?Ä¡W_˜C¿;öðÎ$þá”êíu«å3ï†²G?®¦à.‰3G$¨ñ„Ý×»ë¥F¼¤Õ¯’PØ-ÐnA7t¥J	Ä£Pj¯ý­¼'(¯wÎ[•.ï˜â¡kÔÅ;ÿMBÁK•Ï£'+º—bR>ë¢øåc$À¹Y;¥˜¦õ@ÕfÌj…•¦”[CE¸òþ>}x	›
+ñvieÐóÑÔÁ‚ƒH|„Ä
+oSÆ€´ñm]ö«§òèµ†ôØ.Y'ÍrÀjÇ,KbÊ“é¹„rp÷Á°»U•Õ Ý»ðQUí~ýRüš‰®þc¨Àì©<^Ü§ÈºÕuà‘½HLËÔî†¸•Ñ7ôÚ¾;
