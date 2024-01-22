51046c3f1508a02213c0e563cd2d47a5644a8239
Rizky <rizky05@gmail.com>
2024-01-23 06:53:21 +0700

    wip ed wasm gzip

--- app/web/src/base/page/ed.tsx
+++ app/web/src/base/page/ed.tsx
@@ -3,26 +3,28 @@ import { EdBase } from "../../nova/ed/ed-base";
 import { EDGlobal } from "../../nova/ed/logic/ed-global";
 import { edInitSync } from "../../nova/ed/logic/ed-sync";
 import { Loading } from "../../utils/ui/loading";
+import init from "wasm-gzip";
 
 export default page({
-  url: "/ed/:site_id/:page_id",
-  component: ({}) => {
-    const p = useGlobal(EDGlobal, "EDITOR");
+	url: "/ed/:site_id/:page_id",
+	component: ({}) => {
+		const p = useGlobal(EDGlobal, "EDITOR");
 
-    const w = window as any;
-    if (!w.Y) {
-      (async () => {
-        (window as any).Y = await import("yjs");
-        (window as any).syncronize = (await import("y-pojo")).syncronize;
-        p.render();
-      })();
-      return <Loading note="init" />;
-    }
+		const w = window as any;
+		if (!w.Y) {
+			(async () => {
+				await init();
+				(window as any).Y = await import("yjs");
+				(window as any).syncronize = (await import("y-pojo")).syncronize;
+				p.render();
+			})();
+			return <Loading note="init" />;
+		}
 
-    if (!edInitSync(p)) {
-      return <Loading note="connecting-ws" />;
-    }
+		if (!edInitSync(p)) {
+			return <Loading note="connecting-ws" />;
+		}
 
-    return <EdBase />;
-  },
+		return <EdBase />;
+	},
 });
