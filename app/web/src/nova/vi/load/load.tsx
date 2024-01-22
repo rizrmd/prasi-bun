32c9d3a46c1c019fff5b5a2c92efa31c970ad6a7
Rizky <rizky05@gmail.com>
2024-01-22 23:25:59 +0700

    wip fix

--- app/web/src/nova/vi/load/load.tsx
+++ app/web/src/nova/vi/load/load.tsx
@@ -2,47 +2,50 @@ import { VG } from "../render/global";
 import { viLoadLegacy } from "./load-legacy";
 
 export const viLoad = (vi: VG, arg: { site_id: string; api_url: string }) => {
-  vi.status = "loading";
-  if (vi.on_status_changes) {
-    vi.on_status_changes(vi.status);
-  }
-  vi.site.id = arg.site_id;
-  vi.site.api_url = arg.api_url;
+	vi.status = "loading";
+	if (vi.on_status_changes) {
+		vi.on_status_changes(vi.status);
+	}
+	vi.site.id = arg.site_id;
+	vi.site.api_url = arg.api_url;
 
-  if (!vi.site.api && !vi.site.db) {
-    viLoadLegacy({
-      site: {
-        id: vi.site.id,
-        api_url: vi.site.api_url,
-        api: {
-          get() {
-            return vi.site.api;
-          },
-          set(val) {
-            vi.site.api = val;
-          },
-        },
-        db: {
-          get() {
-            return vi.site.db;
-          },
-          set(val) {
-            vi.site.db = val;
-          },
-        },
-      },
-      render: vi.render,
-    }).then(() => {
-      vi.status = "ready";
-      if (vi.on_status_changes) {
-        vi.on_status_changes(vi.status);
-      }
-      vi.render();
-    });
-  } else {
-    vi.status = "ready";
-    if (vi.on_status_changes) {
-      vi.on_status_changes(vi.status);
-    }
-  }
+	if (!vi.site.api && !vi.site.db) {
+		if (vi.site.code.mode === "old") {
+			viLoadLegacy({
+				site: {
+					id: vi.site.id,
+					api_url: vi.site.api_url,
+					api: {
+						get() {
+							return vi.site.api;
+						},
+						set(val) {
+							vi.site.api = val;
+						},
+					},
+					db: {
+						get() {
+							return vi.site.db;
+						},
+						set(val) {
+							vi.site.db = val;
+						},
+					},
+				},
+				render: vi.render,
+			}).then(() => {
+				vi.status = "ready";
+				if (vi.on_status_changes) {
+					vi.on_status_changes(vi.status);
+				}
+				vi.render();
+			});
+		} else {
+		}
+	} else {
+		vi.status = "ready";
+		if (vi.on_status_changes) {
+			vi.on_status_changes(vi.status);
+		}
+	}
 };
--- app/web/src/nova/vi/render/global.tsx
+++ app/web/src/nova/vi/render/global.tsx
@@ -4,23 +4,26 @@ import { viParts } from "./parts";
 
 type ViStatus = "init" | "loading" | "ready";
 export const ViGlobal = {
-  ts: 0,
-  status: "init" as ViStatus,
-  meta: {} as Record<string, IMeta>,
-  tick: 0,
-  site: {
-    id: "",
-    api_url: "",
-    api: null as any,
-    db: null as any,
-  },
-  script: {
-    init_local_effect: undefined as undefined | Record<string, boolean>,
-  },
-  visit: undefined as
-    | undefined
-    | ((meta: IMeta, parts: ReturnType<typeof viParts>) => void),
-  on_status_changes: undefined as void | ((status: ViStatus) => void),
+	ts: 0,
+	status: "init" as ViStatus,
+	meta: {} as Record<string, IMeta>,
+	tick: 0,
+	site: {
+		id: "",
+		api_url: "",
+		api: null as any,
+		db: null as any,
+		code: {
+			mode: "old" as "old" | "vsc",
+		},
+	},
+	script: {
+		init_local_effect: undefined as undefined | Record<string, boolean>,
+	},
+	visit: undefined as
+		| undefined
+		| ((meta: IMeta, parts: ReturnType<typeof viParts>) => void),
+	on_status_changes: undefined as void | ((status: ViStatus) => void),
 };
 
 export type VG = typeof ViGlobal & { render: () => void };
