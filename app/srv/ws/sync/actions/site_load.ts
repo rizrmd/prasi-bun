ea426c0dc9a23efafda224ac549a4f3c75b83601
Rizky <rizky05@gmail.com>
2024-01-22 23:25:59 +0700

    wip fix

--- app/srv/ws/sync/actions/site_load.ts
+++ app/srv/ws/sync/actions/site_load.ts
@@ -6,43 +6,43 @@ import { activity } from "../entity/activity";
 import { SyncConnection } from "../type";
 
 export const site_load: SAction["site"]["load"] = async function (
-  this: SyncConnection,
-  site_id: string
+	this: SyncConnection,
+	site_id: string,
 ) {
-  if (validate(site_id)) {
-    const site = await db.site.findFirst({ where: { id: site_id } });
-    if (site) {
-      if (this.conf) this.conf.site_id = site.id;
+	if (validate(site_id)) {
+		const site = await db.site.findFirst({ where: { id: site_id } });
+		if (site) {
+			if (this.conf) this.conf.site_id = site.id;
 
-      const config =
-        typeof site.config === "object" && site.config
-          ? { api_url: (site.config as any).api_url || "" }
-          : { api_url: "" };
+			const config =
+				typeof site.config === "object" && site.config
+					? { api_url: (site.config as any).api_url || "" }
+					: { api_url: "" };
 
-      activity.site.room(site.id).join({ ws: this.ws });
+			activity.site.room(site.id).join({ ws: this.ws });
 
-      const layout = await db.page.findFirst({
-        where: {
-          id_site: site_id,
-          is_deleted: false,
-          is_default_layout: true,
-        },
-        select: { id: true },
-      });
+			const layout = await db.page.findFirst({
+				where: {
+					id_site: site_id,
+					is_deleted: false,
+					is_default_layout: true,
+				},
+				select: { id: true },
+			});
 
-      await prepCode(site_id, "site");
+			await prepCode(site_id, "site");
 
-      return {
-        id: site.id,
-        name: site.name,
-        config: config as ESite["config"],
-        domain: site.domain,
-        js: site.js || "",
-        responsive: site.responsive as ESite["responsive"],
-        js_compiled: site.js_compiled || "",
-        layout: { id: layout?.id || "", snapshot: null, meta: undefined },
-        code: { snapshot: null, mode: site.code_mode as "old" | "vsc" },
-      };
-    }
-  }
+			return {
+				id: site.id,
+				name: site.name,
+				config: config as ESite["config"],
+				domain: site.domain,
+				js: site.js || "",
+				responsive: site.responsive as ESite["responsive"],
+				js_compiled: site.js_compiled || "",
+				layout: { id: layout?.id || "", snapshot: null, meta: undefined },
+				code: { snapshot: null, mode: site.code_mode as "old" | "vsc" },
+			};
+		}
+	}
 };
--- app/web/src/nova/ed/panel/popup/code/code.tsx
+++ app/web/src/nova/ed/panel/popup/code/code.tsx
@@ -48,6 +48,12 @@ export const EdPopCode = () => {
 		if (code.mode === "") {
 			db.code.findFirst({ where: { id_site: p.site.id } }).then((e) => {
 				code.mode = e ? "new" : "old";
+
+				if (localStorage.vsc_opened === "yes") {
+					localStorage.removeItem("vsc_opened");
+					p.ui.popup.code.open = true;
+				}
+
 				p.render();
 			});
 		}
@@ -58,6 +64,8 @@ export const EdPopCode = () => {
 			fade={false}
 			open={p.ui.popup.code.open}
 			onOpenChange={(open) => {
+				localStorage.removeItem("vsc_opened");
+
 				if (!open) {
 					p.ui.popup.code.open = false;
 					p.render();
@@ -106,145 +114,187 @@ const CodeBody = () => {
 		? "http://localhost:3000?"
 		: "https://prasi-vsc.avolut.com/?tkn=prasi&";
 
+	const code_mode = p.site.code.mode;
+
 	return (
 		<div className="relative w-full h-full flex flex-col">
-			<div className="border-b flex h-[40px] items-stretch">
-				<Popover
-					placement="bottom"
-					offset={0}
-					arrow={false}
-					backdrop={false}
-					content={
-						<CodeNameList
-							onPick={async (e) => {
-								local.namePicker = false;
-								p.ui.popup.code.name = e.name;
-								p.ui.popup.code.id = "";
-								p.render();
-
-								const id_code = await p.sync.activity("site", {
-									action: "open",
-									id: p.site.id,
-									type: "code",
-									name: p.ui.popup.code.name,
-								});
-
-								if (id_code) {
-									p.ui.popup.code.id = id_code;
+			<div className="border-b flex justify-between h-[40px] items-stretch">
+				<div className="flex items-stretch">
+					<Popover
+						placement="bottom"
+						offset={0}
+						arrow={false}
+						backdrop={false}
+						content={
+							<CodeNameList
+								onPick={async (e) => {
+									local.namePicker = false;
+									p.ui.popup.code.name = e.name;
+									p.ui.popup.code.id = "";
 									p.render();
-								}
-							}}
-						/>
-					}
-					popoverClassName="bg-white shadow-md"
-					className={cx(
-						"flex items-center px-2 w-[200px] hover:bg-blue-50  space-x-1",
-						"cursor-pointer justify-between",
-					)}
-					open={local.namePicker}
-					onOpenChange={(open) => {
-						local.namePicker = open;
-						local.render();
-					}}
-				>
-					<div className="capitalize overflow-ellipsis flex-1 flex items-center space-x-2">
-						<CodeNameItem name={p.ui.popup.code.name} />
-					</div>
-					<div
-						dangerouslySetInnerHTML={{
-							__html: iconChevronDown,
-						}}
-					></div>
-				</Popover>
 
-				{p.ui.popup.code.name !== "site" && p.ui.popup.code.name !== "SSR" && (
-					<>
-						<Tooltip
-							content={"Delete Code Module"}
-							className="flex items-center border-l relative hover:bg-red-50 cursor-pointer px-2 transition-all text-red-500"
-							placement="bottom"
-							onClick={async () => {
-								if (
-									prompt(
-										"Are you sure want to delete this code?\ntype 'yes' to confirm:",
-									) === "yes"
-								) {
-									await db.code.delete({
-										where: { id: p.ui.popup.code.id },
+									const id_code = await p.sync.activity("site", {
+										action: "open",
+										id: p.site.id,
+										type: "code",
+										name: p.ui.popup.code.name,
 									});
 
-									codeName.list = codeName.list.filter(
-										(e) => e.id !== p.ui.popup.code.id,
-									);
+									if (id_code) {
+										p.ui.popup.code.id = id_code;
+										p.render();
+									}
+								}}
+							/>
+						}
+						popoverClassName="bg-white shadow-md"
+						className={cx(
+							"flex items-center px-2 w-[200px] hover:bg-blue-50  space-x-1",
+							"cursor-pointer justify-between",
+						)}
+						open={local.namePicker}
+						onOpenChange={(open) => {
+							local.namePicker = open;
+							local.render();
+						}}
+					>
+						<div className="capitalize overflow-ellipsis flex-1 flex items-center space-x-2">
+							<CodeNameItem name={p.ui.popup.code.name} />
+						</div>
+						<div
+							dangerouslySetInnerHTML={{
+								__html: iconChevronDown,
+							}}
+						></div>
+					</Popover>
 
-									p.ui.popup.code.name = codeName.list[0].name;
-									p.ui.popup.code.id = codeName.list[0].id;
-									p.render();
+					{p.ui.popup.code.name !== "site" &&
+						p.ui.popup.code.name !== "SSR" && (
+							<>
+								<Tooltip
+									content={"Delete Code Module"}
+									className="flex items-center border-l relative hover:bg-red-50 cursor-pointer px-2 transition-all text-red-500"
+									placement="bottom"
+									onClick={async () => {
+										if (
+											prompt(
+												"Are you sure want to delete this code?\ntype 'yes' to confirm:",
+											) === "yes"
+										) {
+											await db.code.delete({
+												where: { id: p.ui.popup.code.id },
+											});
+
+											codeName.list = codeName.list.filter(
+												(e) => e.id !== p.ui.popup.code.id,
+											);
+
+											p.ui.popup.code.name = codeName.list[0].name;
+											p.ui.popup.code.id = codeName.list[0].id;
+											p.render();
+										}
+									}}
+								>
+									<div
+										dangerouslySetInnerHTML={{
+											__html: iconTrash,
+										}}
+									></div>
+								</Tooltip>
+								<Popover
+									open={local.codeAssign}
+									onOpenChange={(open) => {
+										local.codeAssign = open;
+										local.render();
+									}}
+									backdrop={false}
+									placement="bottom"
+									popoverClassName="p-0 shadow-lg bg-white"
+									content={
+										<CodeAssign
+											onClose={() => {
+												local.codeAssign = false;
+												local.render();
+											}}
+											id_code={p.ui.popup.code.id}
+										/>
+									}
+									className="flex items-center border-l relative hover:bg-blue-50 cursor-pointer px-2 transition-all"
+								>
+									<div
+										dangerouslySetInnerHTML={{
+											__html: iconGear,
+										}}
+									></div>
+								</Popover>
+							</>
+						)}
+					<Tooltip
+						content="STDOUT Log"
+						delay={0}
+						placement="bottom"
+						className={cx(
+							"flex items-stretch relative border-l",
+							p.ui.popup.code.error && "bg-red-500 text-white",
+						)}
+						onClick={() => {
+							p.ui.popup.code.show_log = !p.ui.popup.code.show_log;
+							p.render();
+						}}
+					>
+						{p.ui.popup.code.show_log && (
+							<div className="absolute bottom-[-4px] left-0 right-[1px] h-[5px] bg-white"></div>
+						)}
+						<div
+							className={cx(
+								"border-r flex text-center items-center hover:bg-blue-50 cursor-pointer px-2 transition-all",
+								p.ui.popup.code.loading
+									? "border-b-2 border-b-orange-400"
+									: "border-b-2 border-b-transparent",
+							)}
+							dangerouslySetInnerHTML={{
+								__html: p.ui.popup.code.loading ? iconLog : iconLoading,
+							}}
+						></div>
+					</Tooltip>
+				</div>
+				<div className="flex items-center">
+					{code_mode === "vsc" && (
+						<div
+							className="flex items-center p-[2px] px-2 mr-2 cursor-pointer text-[11px] space-x-1 hover:bg-blue-100 hover:border-slate-200 border border-transparent transition-all"
+							onClick={async () => {
+								if (
+									confirm(
+										"Are you sure want to turn off VSCode?\nThis will enable old npm module",
+									)
+								) {
+									localStorage.vsc_opened = "yes";
+									await db.site.update({
+										where: { id: p.site.id },
+										data: { code_mode: "old" },
+									});
+									location.reload();
 								}
 							}}
 						>
-							<div
-								dangerouslySetInnerHTML={{
-									__html: iconTrash,
-								}}
-							></div>
-						</Tooltip>
-						<Popover
-							open={local.codeAssign}
-							onOpenChange={(open) => {
-								local.codeAssign = open;
-								local.render();
-							}}
-							backdrop={false}
-							placement="bottom"
-							popoverClassName="p-0 shadow-lg bg-white"
-							content={
-								<CodeAssign
-									onClose={() => {
-										local.codeAssign = false;
-										local.render();
-									}}
-									id_code={p.ui.popup.code.id}
-								/>
-							}
-							className="flex items-center border-l relative hover:bg-blue-50 cursor-pointer px-2 transition-all"
-						>
-							<div
-								dangerouslySetInnerHTML={{
-									__html: iconGear,
-								}}
-							></div>
-						</Popover>
-					</>
-				)}
-				<Tooltip
-					content="STDOUT Log"
-					delay={0}
-					placement="bottom"
-					className={cx(
-						"flex items-stretch relative border-l",
-						p.ui.popup.code.error && "bg-red-500 text-white",
+							<svg
+								width="15"
+								height="15"
+								viewBox="0 0 15 15"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M10.5 4C8.567 4 7 5.567 7 7.5C7 9.433 8.567 11 10.5 11C12.433 11 14 9.433 14 7.5C14 5.567 12.433 4 10.5 4ZM7.67133 11C6.65183 10.175 6 8.91363 6 7.5C6 6.08637 6.65183 4.82498 7.67133 4H4.5C2.567 4 1 5.567 1 7.5C1 9.433 2.567 11 4.5 11H7.67133ZM0 7.5C0 5.01472 2.01472 3 4.5 3H10.5C12.9853 3 15 5.01472 15 7.5C15 9.98528 12.9853 12 10.5 12H4.5C2.01472 12 0 9.98528 0 7.5Z"
+									fill="currentColor"
+									fillRule="evenodd"
+									clipRule="evenodd"
+								></path>
+							</svg>
+							<div>Turn off VSCode</div>
+						</div>
 					)}
-					onClick={() => {
-						p.ui.popup.code.show_log = !p.ui.popup.code.show_log;
-						p.render();
-					}}
-				>
-					{p.ui.popup.code.show_log && (
-						<div className="absolute bottom-[-4px] left-0 right-[1px] h-[5px] bg-white"></div>
-					)}
-					<div
-						className={cx(
-							"border-r flex text-center items-center hover:bg-blue-50 cursor-pointer px-2 transition-all",
-							p.ui.popup.code.loading
-								? "border-b-2 border-b-orange-400"
-								: "border-b-2 border-b-transparent",
-						)}
-						dangerouslySetInnerHTML={{
-							__html: p.ui.popup.code.loading ? iconLog : iconLoading,
-						}}
-					></div>
-				</Tooltip>
+				</div>
 			</div>
 			{p.ui.popup.code.show_log && (
 				<div className="h-[150px] overflow-auto font-mono p-2 text-xs whitespace-pre-wrap border-b">
@@ -252,21 +302,46 @@ const CodeBody = () => {
 				</div>
 			)}
 
-			<div className="flex flex-1 relative">
-				{!p.ui.popup.code.open || !p.ui.popup.code.id ? (
-					<Loading backdrop={false} />
-				) : (
-					<>
-						<iframe
-							className="flex flex-1 absolute inset-0 w-full h-full z-10"
-							src={`${vscode_url}folder=/site/code/${p.site.id}/${p.ui.popup.code.id}`}
-						></iframe>
-						<div className="flex flex-1 absolute inset-0 z-0 items-center justify-center">
-							Loading VSCode...
-						</div>
-					</>
-				)}
-			</div>
+			{code_mode === "vsc" ? (
+				<div className="flex flex-1 relative">
+					{!p.ui.popup.code.open || !p.ui.popup.code.id ? (
+						<Loading backdrop={false} />
+					) : (
+						<>
+							<iframe
+								className="flex flex-1 absolute inset-0 w-full h-full z-10"
+								src={`${vscode_url}folder=/site/code/${p.site.id}/${p.ui.popup.code.id}`}
+							></iframe>
+							<div className="flex flex-1 absolute inset-0 z-0 items-center justify-center">
+								Loading VSCode...
+							</div>
+						</>
+					)}
+				</div>
+			) : (
+				<div className="flex flex-col flex-1 relative items-center justify-center space-y-2">
+					<div className="text-xs">VSCode is turned off</div>
+					<div
+						className="flex items-center p-2 cursor-pointer text-xs font-mono space-x-1 bg-green-700 text-white hover:opacity-40 transition-all"
+						onClick={async () => {
+							if (
+								confirm(
+									"Are you sure want to turn on VSCode?\nThis will disable old npm module (you can enable it again later).",
+								)
+							) {
+								localStorage.vsc_opened = "yes";
+								await db.site.update({
+									where: { id: p.site.id },
+									data: { code_mode: "vsc" },
+								});
+								location.reload();
+							}
+						}}
+					>
+						<div>Turn on VSCode</div>
+					</div>
+				</div>
+			)}
 
 			{(local.namePicker || local.codeAssign) && (
 				<div
