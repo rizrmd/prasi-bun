3a48d4ad2d68674fad8638b20fec49e6e2b30564
Rizky <rizky05@gmail.com>
2024-02-07 18:40:53 +0700

    wip fix

--- pkgs/core/index.ts
+++ pkgs/core/index.ts
@@ -31,6 +31,7 @@ if (!g.Y) {
   g._api = {};
   g.mode = process.argv.includes("dev") ? "dev" : "prod";
   g.datadir = dir.path(g.mode === "prod" ? "../data" : "data", false);
+  console.log("DataDir", g.datadir);
   g.port = parseInt(process.env.PORT || "4550");
 
   g.log.info(g.mode === "dev" ? "DEVELOPMENT" : "PRODUCTION");
