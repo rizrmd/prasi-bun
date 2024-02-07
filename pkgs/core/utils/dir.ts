98d1de970be9ec0a0010280e5467fb8bc8ed758c
Rizky <rizky05@gmail.com>
2024-02-07 19:15:02 +0700

    wip fix

--- pkgs/core/utils/dir.ts
+++ pkgs/core/utils/dir.ts
@@ -3,6 +3,11 @@ import { join } from "path";
 export const dir = {
   path: (path: string, safe?: boolean) => {
     if (safe === false) return join(process.cwd(), path);
+
+    if (path.startsWith(process.cwd())) {
+      return path;
+    }
+    
     const final_path = path
       .split("/")
       .filter((e) => e !== "..")
