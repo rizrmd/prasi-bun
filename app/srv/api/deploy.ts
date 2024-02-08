5ad4b0323f4cbe0477b4fd45a7000d2cf6bfbe6f
Rizky <rizky05@gmail.com>
2024-02-08 08:51:54 +0700

    wip fix

--- app/srv/api/deploy.ts
+++ app/srv/api/deploy.ts
@@ -18,7 +18,7 @@ export const _ = {
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1.0, minimum-scale=1.0, maximum-scale=1.0">
 <title></title>
 <link rel="stylesheet" href="https://prasi.app/index.css">
 </head>
--- pkgs/core/build-site.ts
+++ pkgs/core/build-site.ts
@@ -41,7 +41,7 @@ await writeAsync(
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1.0, minimum-scale=1.0, maximum-scale=1.0">
   <title></title>
   <link rel="stylesheet" href="https://prasi.app/index.css">
 </head>
