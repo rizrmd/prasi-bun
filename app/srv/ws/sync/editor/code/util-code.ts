8921df00e7876c2de1592bd9199a5437e700e9e4
Rizky <rizky05@gmail.com>
2024-04-17 19:31:22 +0700

    fix server

--- app/srv/ws/sync/editor/code/util-code.ts
+++ app/srv/ws/sync/editor/code/util-code.ts
@@ -81,6 +81,7 @@ declare global {
       index: { head: string[]; body: string[]; render: () => string };
       prasi: { page_id?: string; params?: Record<string, any> };
     }) => Promise<Response>;
+    init: () => Promise<void>;
   };
 }
 `;
