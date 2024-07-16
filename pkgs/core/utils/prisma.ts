2431c8531daa10c65911d4e88cfd19fee70836db
Rizky <rizky@avolut.com>
2024-07-17 02:07:16 +0700

    fix

--- pkgs/core/utils/prisma.ts
+++ pkgs/core/utils/prisma.ts
@@ -8,11 +8,11 @@ export const preparePrisma = async () => {
 		(await existsAsync(dir.path("app/db/.env"))) ||
 		process.env.DATABASE_URL
 	) {
-		// if (g.mode === "prod" && exists(dir.path("app/static-br"))) {
-		// 	g.log.info("Prisma: db pull & generate");
-		// 	await $({ cwd: dir.path(`app/db`) })`bun prisma db pull`;
-		// 	await $({ cwd: dir.path(`app/db`) })`bun prisma generate`;
-		// }
+		if (g.mode === "prod" && exists(dir.path("app/static-br"))) {
+			g.log.info("Prisma: db pull & generate");
+			await $({ cwd: dir.path(`app/db`) })`bun prisma db pull`;
+			await $({ cwd: dir.path(`app/db`) })`bun prisma generate`;
+		}
 
 		const { PrismaClient } = await import("../../../app/db/db");
 		g._db = new PrismaClient();
