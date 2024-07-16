b27a7e141db51eba3ff3ccf90560bf88bd93484f
Rizky <rizky@avolut.com>
2024-07-17 01:22:59 +0700

    fix

--- app/web/src/font.ts
+++ app/web/src/font.ts
@@ -1,2 +1,8 @@
-import "@fontsource/jetbrains-mono";
-import "@fontsource/source-sans-3";
+//@ts-nocheck
+import f1 from "bundle-text:@fontsource/jetbrains-mono/index.css";
+import f2 from "bundle-text:@fontsource/source-sans-3/index.css";
+
+let style = document.createElement("style");
+style.textContent = f1.replaceAll(`url("`, `url("/`) + f2.replaceAll(`url("`, `url("/`);
+style.id = "font";
+document.head.appendChild(style);
