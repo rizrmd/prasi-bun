adf0be5c23e1ca6a1ba8658193e45ed74fbc9c4e
Rizky <rizky@avolut.com>
2024-07-27 18:38:38 +0700

    fix

--- app/web/src/nova/ed/panel/tree/scroll-tree.ts
+++ app/web/src/nova/ed/panel/tree/scroll-tree.ts
@@ -2,16 +2,16 @@ import { active } from "../../logic/ed-global";
 
 export const scrollTreeActiveItem = () => {
   setTimeout(() => {
-    let i = 0;
-    const ival = setInterval(() => {
-      const div = document.querySelector(
-        `.tree-${active.item_id}`
-      ) as HTMLDivElement;
-      if (div) {
-        if (!active.hover.tree) div.scrollIntoView();
-      }
-      if (i > 7) clearInterval(ival);
-      i++;
-    }, 30);
+    // let i = 0;
+    // const ival = setInterval(() => {
+    const div = document.querySelector(
+      `.tree-${active.item_id}`
+    ) as HTMLDivElement;
+    if (div) {
+      if (!active.hover.tree) div.scrollIntoView();
+    }
+    //   if (i > 7) clearInterval(ival);
+    //   i++;
+    // }, 30);
   }, 100);
 };
