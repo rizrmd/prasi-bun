e45fd18fa335ac5575db797ebb62c43d4f65f11c
Rizky <rizky@avolut.com>
2024-08-02 18:02:51 +0700

    fix adv js

--- app/web/src/nova/ed/panel/popup/script/code-edit/adv-js.ts
+++ app/web/src/nova/ed/panel/popup/script/code-edit/adv-js.ts
@@ -5,22 +5,28 @@ import { active, PG } from "../../../../logic/ed-global";
 export const codeEditAdvJs = (p: PG, value: string) => {
   const meta = getMetaById(p, active.item_id);
 
-  if (meta) {
-    const mprop = meta.mitem?.get("adv");
-    if (mprop) {
-      try {
-        const valueBuilt = codeBuild(
-          { _: `render (${value})` },
-          `[item: ${meta.item.name} - ${meta.item.id}]`
-        )["_"];
+  if (meta && meta.mitem) {
+    meta.mitem.doc?.transact(() => {
+      let mprop = meta.mitem?.get("adv");
 
-        mprop.doc?.transact(() => {
+      if (!mprop) {
+        meta.mitem?.set("adv", new Y.Map() as any);
+        mprop = meta.mitem?.get("adv");
+      }
+
+      if (mprop) {
+        try {
+          const valueBuilt = codeBuild(
+            { _: `render (${value})` },
+            `[item: ${meta.item.name} - ${meta.item.id}]`
+          )["_"];
+
           mprop.set("js", value);
           mprop.set("jsBuilt", valueBuilt);
-        });
-      } catch (e: any) {
-        return e.message;
+        } catch (e: any) {
+          return e.message;
+        }
       }
-    }
+    });
   }
 };
