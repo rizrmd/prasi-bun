8b81e6d9329f0727795a7219d718cd410ab45819
Rizky <rizky@avolut.com>
2024-08-06 19:46:31 +0700

    fix

--- app/srv/api/type-rebuild.ts
+++ app/srv/api/type-rebuild.ts
@@ -1,12 +1,14 @@
 import { apiContext } from "service-srv";
 import { initTypings } from "../ws/sync/code/parts/init/typings";
-
+import { dir } from "dir";
+import { removeAsync } from "fs-jetpack";
 export const _ = {
   url: "/type-rebuild/:id_site",
   async api(id_site: string) {
     const { req, res } = apiContext(this);
     const root = `/code/${id_site}/site/src`;
 
+    await removeAsync(dir.data(`${root}/typings`));
     await initTypings(root, id_site, true);
 
     return "ok";
