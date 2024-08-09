90a19fd34a99b5238e3e6d414522e57397ecae99
Rizky <rizky@avolut.com>
2024-08-09 15:22:11 +0700

    fix

--- app/srv/api/rebuild.ts
+++ app/srv/api/rebuild.ts
@@ -1,18 +1,16 @@
 import { code } from "../ws/sync/code/code";
 import { initFrontEnd } from "../ws/sync/code/parts/init/frontend";
-import { initServer } from "../ws/sync/code/parts/init/server";
-import { initTypings } from "../ws/sync/code/parts/init/typings";
 
 export const _ = {
   url: "/rebuild/:id_site",
   async api(id_site: string) {
     const { frontend, server, typings } = code.internal;
     const root = `/code/${id_site}/site/src`;
-    delete frontend[id_site];
-    delete server[id_site];
+    // delete frontend[id_site];
+    // delete server[id_site];
     await initFrontEnd(root, id_site, true);
-    await initServer(root, id_site, true);
-    await initTypings(root, id_site, true);
+    // await initServer(root, id_site, true);
+    // await initTypings(root, id_site, true);
 
     return "ok";
   },
