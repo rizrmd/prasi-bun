1c0aae094d1a58d657bd2f7bc7818fbe4daaf410
Rizky <rizky@avolut.com>
2024-11-22 04:29:38 +0700

    fix

--- app/web/src/nova/ed/panel/header/right/responsive-toggle.tsx
+++ app/web/src/nova/ed/panel/header/right/responsive-toggle.tsx
@@ -65,7 +65,6 @@ export const ResponsiveToggle = () => {
             d="M4 2.5a.5.5 0 01.5-.5h6a.5.5 0 01.5.5v10a.5.5 0 01-.5.5h-6a.5.5 0 01-.5-.5v-10zM4.5 1A1.5 1.5 0 003 2.5v10A1.5 1.5 0 004.5 14h6a1.5 1.5 0 001.5-1.5v-10A1.5 1.5 0 0010.5 1h-6zM6 11.65a.35.35 0 100 .7h3a.35.35 0 100-.7H6z"
             clipRule="evenodd"
           ></path>
-          {mroot?.get("responsive")}
         </svg>
       ),
     },
@@ -77,7 +76,7 @@ export const ResponsiveToggle = () => {
           render();
           return;
         }
-        
+
         p.mode = "desktop";
         w.isMobile = false;
         w.isDesktop = true;
