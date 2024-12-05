0a0cf3a1e5b004ce9a1e3f9df8a8835fd1ea9d59
Rizky <rizky@avolut.com>
2024-12-05 12:14:30 +0700

    fix responsive toggle

--- static/sworker.js
+++ static/sworker.js
@@ -175,12 +175,12 @@ const manifest = [
     "/source-sans-3-latin-ext-400-normal.0483ba2f.woff",
     "/source-sans-3-latin-400-normal.43f5aafe.woff2",
     "/source-sans-3-latin-400-normal.6f62a854.woff",
-    "/index.f0ccd585.js",
+    "/index.785f3a52.js",
     "/login.3daaefac.js",
     "/logout.920e94f5.js",
     "/register.5db9c712.js",
     "/all.5b92210c.js",
-    "/ed.a9947ad3.js",
+    "/ed.06f8ddc8.js",
     "/wasm_gzip.922c2de1.wasm",
     "/standalone.0aea92ae.js",
     "/estree.4a6560c1.js",
@@ -190,7 +190,7 @@ const manifest = [
     "/index.module.7282dc17.js",
     "/ed.400f6bfd.css"
 ];
-const version = "6c3ec44e";
+const version = "d4c2fe24";
 (0, _serviceWorker._register)(manifest, version);
 
 },{"@parcel/service-worker":"et5Hi"}],"et5Hi":[function(require,module,exports) {
@@ -564,7 +564,7 @@ function _routerNodeToTable(initialPath, initialNode) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "version", ()=>version);
-const version = "crcdsb6";
+const version = "zpqajfx";
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"jpDjm"}],"lXNsG":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
